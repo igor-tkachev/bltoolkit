d9779801f17fbdaea824c9fc9e1a2c7e64293472
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2005-05-31 03:19:32 +0000

    Added a folder remotely
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@14 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ 1/Examples/DbManager/ExecuteDictionary_IDictionary_string_Type.cs
@@ -0,0 +1,49 @@
+/// example:
+/// db ExecuteDictionary(Hashtable,string,Type)
+/// comment:
+/// The following example demonstrates how to use the <b>ExecuteDictionary</b> method.
+using System;
+using System.Collections;
+
+using NUnit.Framework;
+
+using Rsdn.Framework.Data;
+
+namespace Examples_DbManager
+{
+	[TestFixture]
+	public class ExecuteDictionary_Hashtable_string_Type
+	{
+		public class Category
+		{
+			public int    ID;
+			public string Name;
+			public string Description;
+		}
+
+		[Test]
+		public void Test()
+		{
+			using (DbManager db = new DbManager())
+			{
+				Hashtable ht = new Hashtable();
+
+				db
+					.SetCommand(@"
+						SELECT
+							CategoryID   as ID,
+							CategoryName as Name,
+							Description  as Description
+						FROM Categories")
+					.ExecuteDictionary(ht, "ID", typeof(Category));
+                    
+				foreach (Category category in ht.Values)
+				{
+					Console.WriteLine("ID  : {0}\nName: {1}\nDesc: {2}",
+						category.ID, category.Name, category.Description);
+					Console.WriteLine();
+				}
+			}
+		}
+	}
+}
