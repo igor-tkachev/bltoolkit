a91ab3729a2160741adb4ec5012b80bc21102c0c
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2005-08-21 18:10:36 +0000

    Методы ExecuteBizEntity переименованы в ExecuteObject. Убрано наследование от Component. Добавлен новый toy project Rsdn.Framework.DataAccess.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk/1@64 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ Toys/DataAccess/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rsdn.Framework.DataAccess.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rsdn.Framework.DataAccess.2")]
+[assembly: AssemblyCopyright("Copyright ©  2005")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM componenets.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4954aa89-a9f6-4453-8901-b9c8f189ffe0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
