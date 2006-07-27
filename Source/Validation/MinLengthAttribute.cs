30b48e69c5a10be944dcae305f2618f39fcbc63e
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2006-07-27 16:36:04 +0000

    Убрал sealed.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk/2@323 827d3e3c-cb35-0410-a708-6b02db8f8db3

--- Source/Validation/MinLengthAttribute.cs
+++ Source/Validation/MinLengthAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace BLToolkit.Validation
 {
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-	public sealed class MinLengthAttribute : ValidatorBaseAttribute
+	public class MinLengthAttribute : ValidatorBaseAttribute
 	{
 		public MinLengthAttribute(int minLength)
 		{
