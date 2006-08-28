8cd43682ed4359897b17a72a6093f0f96707c2ce
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2006-08-28 02:46:02 +0000

    BLToolkit update.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@351 827d3e3c-cb35-0410-a708-6b02db8f8db3

--- 2/Source/Properties/AssemblyInfo.cs
+++ 2/Source/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Diagnostics.CodeAnalysis;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 
 // FxCop
 
