c7d70439ae8e1ca98e88235e01f19a62c0629edb
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2006-10-22 20:24:00 +0000

    Version incrementation.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@383 827d3e3c-cb35-0410-a708-6b02db8f8db3

--- 2/Source/Properties/AssemblyInfo.cs
+++ 2/Source/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Diagnostics.CodeAnalysis;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.3.0")]
-[assembly: AssemblyFileVersion("2.0.3.61005")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.61005")]
 
 // FxCop
 
