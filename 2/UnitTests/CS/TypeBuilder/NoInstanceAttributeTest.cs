f57072ee4566ac3a72126d9faa60db637a5b8a83
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2006-11-01 18:41:55 +0000

    Добавил юнит тест для NoInstanceAttributeTest.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@388 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ 2/UnitTests/CS/TypeBuilder/NoInstanceAttributeTest.cs
@@ -0,0 +1,35 @@
+using System;
+
+using NUnit.Framework;
+
+using BLToolkit.Reflection;
+using BLToolkit.TypeBuilder;
+
+namespace TypeBuilder
+{
+	[TestFixture]
+	public class NoInstanceAttributeTest
+	{
+		public abstract class PersonCitizenship
+		{
+		}
+
+		public abstract partial class Person
+		{
+			[NoInstance]
+			public abstract PersonCitizenship Citizenship { get; set; }
+		}
+
+		[Test]
+		public void Text()
+		{
+			Person person = (Person)TypeAccessor.CreateInstance(typeof(Person));
+
+			Assert.IsNull(person.Citizenship);
+
+			person.Citizenship = (PersonCitizenship)TypeAccessor.CreateInstance(typeof(PersonCitizenship));
+
+			Assert.IsNotNull(person.Citizenship);
+		}
+	}
+}
