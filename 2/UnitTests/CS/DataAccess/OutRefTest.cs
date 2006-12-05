81ee0cc6fa120aebfdbc2cef4e5ffd289c07fd82
pbludov <pbludov@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2006-12-05 04:42:52 +0000

    Treat DBNull as null.         http://rsdn.ru/Forum/Message.aspx?mid=2248153
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@411 827d3e3c-cb35-0410-a708-6b02db8f8db3

--- 2/UnitTests/CS/DataAccess/OutRefTest.cs
+++ 2/UnitTests/CS/DataAccess/OutRefTest.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 using BLToolkit.DataAccess;
 using BLToolkit.TypeBuilder;
 
-namespace DataAccess
+namespace A.DataAccess
 {
 	[TestFixture]
 	public class OutRefTest
@@ -29,6 +29,13 @@ namespace DataAccess
 		{
 			public abstract void OutRefTest(
 				int    @ID,  out int    @outputID,  ref int    @inputOutputID,
+				string @str, out string @outputStr, ref string @inputOutputStr);
+
+			[SprocName("OutRefTest")]
+			public abstract void OutRefTest2(
+				[ParamNullValue(-1)] int     @ID,
+				[ParamNullValue(-2)] out int @outputID,
+				[ParamNullValue(-3)] ref int @inputOutputID,
 				string @str, out string @outputStr, ref string @inputOutputStr);
 
 			public abstract void OutRefTest(
@@ -64,10 +71,92 @@ namespace DataAccess
 			((TestAccessor)DataAccessor.CreateInstance(typeof(TestAccessor)))
 				.OutRefTest(out @outputID, e, ref @inputOutputStr);
 
-			Assert.AreEqual(5,     @outputID);
-			Assert.AreEqual(10,    e.inputOutputID);
-			Assert.AreEqual(null,  e.outputStr);
+			Assert.AreEqual(5,    @outputID);
+			Assert.AreEqual(10,   e.inputOutputID);
+			Assert.AreEqual(null, e.outputStr);
 			Assert.AreEqual("10", @inputOutputStr);
 		}
+
+		//PB61205: TBD. Implement ParamNullValue for output parameters
+		//
+		//[Test] 
+		public void NullValueTest()
+		{
+			int    @outputID;
+			int    @inputOutputID = 10;
+			string @outputStr;
+			string @inputOutputStr = "10";
+
+			((TestAccessor)DataAccessor.CreateInstance(typeof(TestAccessor)))
+				.OutRefTest2(-1, out @outputID,  ref @inputOutputID,
+				            "5", out @outputStr, ref @inputOutputStr);
+
+			Assert.AreEqual(-2, @outputID);
+			Assert.AreEqual(-3, @inputOutputID);
+		}
+
+#if FW2
+
+		public class NullableEntity
+		{
+			public int?   ID             = null;
+			public int?   outputID;
+			public int?   inputOutputID  = 20;
+			public string str            = null;
+			public string outputStr;
+			public string inputOutputStr = "40";
+		}
+
+		public abstract class TestNullableAccessor : DataAccessor
+		{
+			public abstract void OutRefTest(
+				int?   @ID,  out int?   @outputID,  ref int?   @inputOutputID,
+				string @str, out string @outputStr, ref string @inputOutputStr);
+
+			public abstract void OutRefTest(
+				out int? @outputID,
+				[Direction.Output("outputStr"), Direction.InputOutput("inputOutputID")] NullableEntity entity,
+				ref string @inputOutputStr);
+
+		}
+
+		[Test]
+		public void TestNullable1()
+		{
+			int?   @outputID;
+			int?   @inputOutputID = 10;
+			string @outputStr;
+			string @inputOutputStr = "10";
+
+			DataAccessor.CreateInstance<TestNullableAccessor>()
+				.OutRefTest(null, out @outputID,  ref @inputOutputID,
+				            null, out @outputStr, ref @inputOutputStr);
+
+			Assert.IsNull (@outputID);
+			Assert.IsNull (@inputOutputID);
+			Assert.IsEmpty(@outputStr);
+			Assert.IsEmpty(@inputOutputStr);
+		}
+
+		[Test]
+		public void TestNullable2()
+		{
+			NullableEntity e = new NullableEntity();
+			int?   @outputID;
+			string @inputOutputStr = "20";
+
+			DataAccessor.CreateInstance<TestNullableAccessor>()
+				.OutRefTest(out @outputID, e, ref @inputOutputStr);
+
+			Assert.IsNull  (@outputID);
+			Assert.IsEmpty (e.outputStr);
+			Assert.IsNull  (e.inputOutputID);
+
+			// No error here. The 'inputOutputStr' field was initialized from 'e'.
+			//
+			Assert.AreEqual("40", @inputOutputStr);
+		}
+#endif
+
 	}
 }
