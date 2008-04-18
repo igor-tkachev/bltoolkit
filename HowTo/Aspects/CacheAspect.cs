59ff3c0f557515e5f34501e22f328ff37b06d76f
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2008-04-18 02:26:55 +0000

    HowTos
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@564 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ HowTo/Aspects/CacheAspect.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+
+using NUnit.Framework;
+
+using BLToolkit.Aspects;
+using BLToolkit.Reflection;
+
+namespace HowTo.Aspects
+{
+	public /*[a]*/abstract/*[/a]*/ class TestClass
+	{
+		public static int Value;
+
+		[/*[a]*/Cache/*[/a]*/(MaxCacheTime=500, IsWeak=false)]
+		public /*[a]*/virtual/*[/a]*/ int CachedMethod(int p1, int p2)
+		{
+			return Value;
+		}
+
+		public static TestClass CreateInstance()
+		{
+			// Use TypeAccessor to create an instance of an abstract class.
+			//
+			return /*[a]*/TypeAccessor/*[/a]*/<TestClass>.CreateInstance();
+		}
+	}
+
+	[TestFixture]
+	public class CacheAspectTest
+	{
+		[Test]
+		public void Test1()
+		{
+			TestClass tc = TestClass.CreateInstance();
+
+			DateTime begin = DateTime.Now;
+
+			// Initial setup for the test static variable.
+			//
+			TestClass.Value = 777;
+
+			while (tc.CachedMethod(2, 2) == 777)
+			{
+				// This change will not affect the Test method return value for 500 ms.
+				//
+				TestClass.Value++;
+			}
+
+			Assert.IsTrue((DateTime.Now - begin).TotalMilliseconds >= 500);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			TestClass tc = TestClass.CreateInstance();
+
+			// Return value depends on parameter values.
+			//
+			TestClass.Value = 1; Assert.AreEqual(1, tc.CachedMethod(1, 1));
+			TestClass.Value = 2; Assert.AreEqual(1, tc.CachedMethod(1, 1)); // no change
+			TestClass.Value = 3; Assert.AreEqual(3, tc.CachedMethod(2, 1));
+
+			// However we can clear cache manually.
+			// For particular method:
+			//
+			CacheAspect.ClearCache(typeof(TestClass), "CachedMethod", typeof(int), typeof(int));
+			TestClass.Value = 4; Assert.AreEqual(4, tc.CachedMethod(2, 1));
+
+			// By MethodInfo:
+			//
+			MethodInfo methodInfo = tc.GetType().GetMethod("CachedMethod", new Type[] { typeof(int), typeof(int) });
+			CacheAspect.ClearCache(methodInfo);
+			TestClass.Value = 5; Assert.AreEqual(5, tc.CachedMethod(2, 1));
+
+			// For the all cached methods.
+			//
+			CacheAspect.ClearCache();
+			TestClass.Value = 6; Assert.AreEqual(6, tc.CachedMethod(2, 1));
+		}
+	}
+}
---
+++ HowTo/Aspects/CustomCacheAspect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+using NUnit.Framework;
+
+using BLToolkit.Aspects;
+using BLToolkit.Reflection;
+
+namespace HowTo.Aspects
+{
+	[TestFixture]
+	public class CustomCacheAspectTest
+	{
+		public class /*[a]*/CustomCacheAspect/*[/a]*/ : CacheAspect
+		{
+			private static IDictionary _cache = new Hashtable();
+			public  static IDictionary  Cache
+			{
+				get { return _cache; }
+			}
+
+			protected /*[a]*/override/*[/a]*/ IDictionary GetCache(InterceptCallInfo info)
+			{
+				return Cache;
+			}
+		}
+
+		public /*[a]*/abstract/*[/a]*/ class CustomTestClass
+		{
+			public static int Value;
+
+			[Cache(/*[a]*/typeof(CustomCacheAspect)/*[/a]*/, MaxSeconds = 1)]
+			public /*[a]*/virtual/*[/a]*/ int CachedMethod(int i1, int i2)
+			{
+				return Value;
+			}
+		}
+
+		[Test]
+		public void Test()
+		{
+			CustomTestClass t = TypeAccessor<CustomTestClass>.CreateInstance();
+
+			CustomTestClass.Value = 1; Assert.AreEqual(1, t.CachedMethod(1, 1));
+			CustomTestClass.Value = 2; Assert.AreEqual(1, t.CachedMethod(1, 1));
+			CustomTestClass.Value = 3; Assert.AreEqual(3, t.CachedMethod(2, 1));
+
+			CustomCacheAspect.Cache.Clear();
+			CustomTestClass.Value = 4; Assert.AreEqual(4, t.CachedMethod(2, 1));
+		}
+	}
+}
