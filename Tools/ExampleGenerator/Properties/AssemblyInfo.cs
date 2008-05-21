31432a966f42550dfb3da9868d4be544caae6e26
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2008-05-21 14:20:00 +0000

    Switch to 3.1.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@598 827d3e3c-cb35-0410-a708-6b02db8f8db3

--- Tools/ExampleGenerator/Properties/AssemblyInfo.cs
+++ Tools/ExampleGenerator/Properties/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
