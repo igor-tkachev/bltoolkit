da1fce888a628d26dd61f37061a5162f0e5ed6d3
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2008-12-23 21:34:05 +0000

    Working on SqlBuilder.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@670 827d3e3c-cb35-0410-a708-6b02db8f8db3

--- Source/Data/Sql/Table.cs
+++ Source/Data/Sql/Table.cs
@@ -58,6 +58,9 @@ namespace BLToolkit.Data.Sql
 
 			foreach (Field field in table.Fields.Values)
 				Fields.Add(new Field(field.Name, field.PhysicalName));
+
+			foreach (Join join in table.Joins)
+				Joins.Add(join.Clone());
 		}
 
 		public Table(Table table)
@@ -69,25 +72,59 @@ namespace BLToolkit.Data.Sql
 
 		#region Init from XML
 
-		public Table(MappingSchema mappingSchema, TypeExtension extension)
-			: this(mappingSchema)
+		public Table(ExtensionList extensions, string name)
+			: this(Map.DefaultSchema, extensions, name)
 		{
-			if (extension == null)               throw new ArgumentNullException("extension");
-			if (extension == TypeExtension.Null) throw new InvalidOperationException("Table not found.");
-
-			_name         = extension.Name;
-			_alias        = (string)extension.Attributes["Alias"].       Value;
-			_database     = (string)extension.Attributes["Database"].    Value;
-			_owner        = (string)extension.Attributes["Owner"].       Value;
-			_physicalName = (string)extension.Attributes["PhysicalName"].Value;
-
-			foreach (MemberExtension me in extension.Members)
-				Fields.Add(new Field(me.Name, (string)me["MapField"].Value ?? (string)me["PhysicalName"].Value));
 		}
 
-		public Table(TypeExtension extension)
-			: this(Map.DefaultSchema, extension)
+		public Table(MappingSchema mappingSchema, ExtensionList extensions, string name)
+			: this(mappingSchema)
 		{
+			if (extensions == null) throw new ArgumentNullException("extensions");
+			if (name       == null) throw new ArgumentNullException("name");
+
+			TypeExtension te = extensions[name];
+
+			if (te == TypeExtension.Null)
+				throw new ArgumentException(string.Format("Table '{0}' not found.", name));
+
+			_name         = te.Name;
+			_alias        = (string)te.Attributes["Alias"].       Value;
+			_database     = (string)te.Attributes["Database"].    Value;
+			_owner        = (string)te.Attributes["Owner"].       Value;
+			_physicalName = (string)te.Attributes["PhysicalName"].Value;
+
+			foreach (MemberExtension me in te.Members)
+				Fields.Add(new Field(me.Name, (string)me["MapField"].Value ?? (string)me["PhysicalName"].Value));
+
+			foreach (AttributeExtension ae in te.Attributes["Join"])
+				Joins.Add(new Join(ae));
+
+			string baseExtension = (string)te.Attributes["BaseExtension"].Value;
+
+			if (!string.IsNullOrEmpty(baseExtension))
+				InitFromBase(new Table(mappingSchema, extensions, baseExtension));
+
+			string baseTypeName = (string)te.Attributes["BaseType"].Value;
+
+			if (!string.IsNullOrEmpty(baseTypeName))
+				InitFromBase(new Table(mappingSchema, Type.GetType(baseTypeName, true, true)));
+		}
+
+		void InitFromBase(Table baseTable)
+		{
+			if (_alias        == null) _alias        = baseTable._alias;
+			if (_database     == null) _database     = baseTable._database;
+			if (_owner        == null) _owner        = baseTable._owner;
+			if (_physicalName == null) _physicalName = baseTable._physicalName;
+
+			foreach (Field field in baseTable.Fields.Values)
+				if (!Fields.ContainsKey(field.Name))
+					Fields.Add(new Field(field.Name, field.PhysicalName));
+
+			foreach (Join join in baseTable.Joins)
+				if (Joins.Find(delegate(Join j) { return j.TableName == join.TableName; }) == null)
+					Joins.Add(join);
 		}
 
 		#endregion
@@ -116,6 +153,9 @@ namespace BLToolkit.Data.Sql
 
 		private ChildContainer<Table,Field> _fields;
 		public  ChildContainer<Table,Field>  Fields { get { return _fields; } }
+
+		private List<Join> _joins = new List<Join>();
+		public  List<Join>  Joins { get { return _joins; } }
 
 		#endregion
 
