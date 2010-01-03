097deb38d0a7aa7c970ec9d9e08aef8689c2378e
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2010-01-03 16:24:37 +0000

    Missing file.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@866 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ UnitTests/Linq/ExistsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Data.Linq
+{
+	[TestFixture]
+	public class ExistsTest : TestBase
+	{
+		[Test]
+		public void SingleOrDefaultWhere()
+		{
+			ForEachProvider(db =>
+				AreEqual(
+					   Parent.Where(p => db.Child.Where(c => c.ParentID == p.ParentID).Any(c => c.ParentID > 3)),
+					db.Parent.Where(p => db.Child.Where(c => c.ParentID == p.ParentID).Any(c => c.ParentID > 3))));
+		}
+	}
+}
