2e117f8e1d081a5034d23ed417e29d394a10a4f2
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2010-06-22 02:39:34 +0000

    Linq demo update.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@980 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ Demo/Linq/Demo/DataModel/NorthwindDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BLToolkit.Data.Linq;
+using BLToolkit.Mapping;
+
+namespace Linq.Demo.DataModel
+{
+	partial class NorthwindDB
+	{
+		public Table<DiscontinuedProduct> DiscontinuedProduct { get { return GetTable<DiscontinuedProduct>(); } }
+		public Table<ActiveProduct>       ActiveProduct       { get { return GetTable<ActiveProduct>();       } }
+	}
+
+	[InheritanceMapping(Code="True",  Type=typeof(DiscontinuedProduct))]
+	[InheritanceMapping(Code="False", Type=typeof(ActiveProduct))]
+	abstract partial class Product
+	{
+	}
+
+	public class ActiveProduct       : Product {}
+	public class DiscontinuedProduct : Product {}
+}
