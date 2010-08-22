fe29ed461b3bb63b710d788bca9e87f3e816cc45
igor.tkachev <igor.tkachev@827d3e3c-cb35-0410-a708-6b02db8f8db3>
2010-08-22 20:32:10 +0000

    Working on SL support.
    git-svn-id: https://bl-toolkit.googlecode.com/svn/trunk@1017 827d3e3c-cb35-0410-a708-6b02db8f8db3

---
+++ Demo/Silverlight/Client/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+	public partial class MainPage : UserControl
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			using (var dm = new DataModel())
+			{
+				dm.Parent.ToList();
+			}
+		}
+	}
+}
