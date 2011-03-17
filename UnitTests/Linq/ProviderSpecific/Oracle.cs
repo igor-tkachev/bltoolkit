052b2f7e0b92277368db9f0fd5e559205e39d398
Igor Tkachev <igor.tkachev@gmail.com>
2011-03-17 16:15:48 -0400

    Test for http://code.google.com/p/bl-toolkit/issues/detail?id=47

--- UnitTests/Linq/ProviderSpecific/Oracle.cs
+++ UnitTests/Linq/ProviderSpecific/Oracle.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Data.Linq.Mapping;
 using System.Linq;
 
 using BLToolkit.Data.Linq;
-
+using BLToolkit.Mapping;
 using NUnit.Framework;
 
 using UnitTests.Linq.Interface.Model;
@@ -43,6 +44,39 @@ namespace Data.Linq.ProviderSpecific
 				db.GetTable<OracleSpecific.SequenceTest>().Where(_ => _.ID == id1).Delete();
 
 				Assert.AreEqual(0, db.GetTable<OracleSpecific.SequenceTest>().Count(_ => _.Value == "SeqValue"));
+			}
+		}
+
+		[Table(Name = "stg_trade_information")]
+		public class Trade
+		{
+			[MapField("STG_TRADE_ID")]          public int       ID             { get; set; }
+			[MapField("STG_TRADE_VERSION")]     public int       Version        { get; set; }
+			[MapField("INFORMATION_TYPE_ID")]   public int       TypeID         { get; set; }
+			[MapField("INFORMATION_TYPE_NAME")] public string    TypeName       { get; set; }
+			[MapField("value")]                 public string    Value          { get; set; }
+			[MapField("value_as_integer")]      public int?      ValueAsInteger { get; set; }
+			[MapField("value_as_date")]         public DateTime? ValueAsDate    { get; set; }
+		}
+
+		[Test]
+		public void InsertBatch()
+		{
+			var data = new[]
+			{
+				new Trade { ID = 375, Version = 1, TypeID = 20224, TypeName = "Gas Month",     },
+				new Trade { ID = 328, Version = 1, TypeID = 20224, TypeName = "Gas Month",     },
+				new Trade { ID = 348, Version = 1, TypeID = 20224, TypeName = "Gas Month",     },
+				new Trade { ID = 357, Version = 1, TypeID = 20224, TypeName = "Gas Month",     },
+				new Trade { ID = 371, Version = 1, TypeID = 20224, TypeName = "Gas Month",     },
+				new Trade { ID = 333, Version = 1, TypeID = 20224, TypeName = "Gas Month",     ValueAsInteger = 1,          ValueAsDate = new DateTime(2011, 1, 5) },
+				new Trade { ID = 353, Version = 1, TypeID = 20224, TypeName = "Gas Month",     ValueAsInteger = 1000000000,                                        },
+				new Trade { ID = 973, Version = 1, TypeID = 20160, TypeName = "EU Allowances", },
+			};
+
+			using (var db = new TestDbManager("Oracle"))
+			{
+				db.InsertBatch(5, data);
 			}
 		}
 	}
