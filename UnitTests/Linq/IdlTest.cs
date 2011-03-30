cd61122dea5ecd6d98d3c0ef85cff890de55d659
Pavel Popov <pavel.popov@idl-corp.biz>
2011-03-30 12:57:08 +0300

    Task #776

--- UnitTests/Linq/IdlTest.cs
+++ UnitTests/Linq/IdlTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using BLToolkit.Data.DataProvider;
 using Data.Linq.Model;
 using NUnit.Framework;
 
@@ -12,36 +14,47 @@ namespace Data.Linq
             public int Value { get; set; }
         }
 
+        private void TestJoin(Action<ITestDataContext> func)
+        {
+            ForEachProvider(
+                new[]
+                    {
+                        "Sql2008", "Sql2005", ProviderName.SqlCe, ProviderName.DB2, ProviderName.Informix,
+                        ProviderName.Firebird, "Oracle", ProviderName.PostgreSQL, ProviderName.SQLite, ProviderName.Sybase
+                        , ProviderName.Access
+                    }, func);
+        }
+
         // fail
         [Test]
         public void Join1()
         {
-            ForEachProvider(db =>
-                {
-                    var query1 = from p1 in db.Person
-                    join p2 in db.Person on p1.ID equals p2.ID
-                    select new
-                        {
-                            ID1 = new ObjectID {Value = p1.ID},
-                            ID2 = new ObjectID {Value = p2.ID},
-                            FirstName1 = p1.FirstName, 
-                            FirstName2 = p2.FirstName, 
-                            Gender1 = p1.Gender,
-                            Gender2 = p2.Gender
-                        };
+            TestJoin(db =>
+                    {
+                        var query1 = from p1 in db.Person
+                                     join p2 in db.Person on p1.ID equals p2.ID
+                                     select new
+                                         {
+                                             ID1 = new ObjectID { Value = p1.ID },
+                                             ID2 = new ObjectID { Value = p2.ID },
+                                             FirstName1 = p1.FirstName,
+                                             FirstName2 = p2.FirstName,
+                                             Gender1 = p1.Gender,
+                                             Gender2 = p2.Gender
+                                         };
 
-                    var query2 = from p1 in query1
-                                 select p1.ID1.Value;
+                        var query2 = from p1 in query1
+                                     select p1.ID1.Value;
 
-                    var result = query2.ToArray();
-                });
+                        var result = query2.ToArray();
+                    });
         }
 
         // fail
         [Test]
         public void Join2()
         {
-            ForEachProvider(db =>
+            TestJoin(db =>
             {
                 var query1 = from p1 in db.Person
                              join p2 in db.Person on p1.ID equals p2.ID
@@ -66,7 +79,7 @@ namespace Data.Linq
         [Test]
         public void Join3()
         {
-            ForEachProvider(db =>
+            TestJoin(db =>
             {
                 var query1 = from p1 in db.Person
                              join p2 in db.Person on p1.ID equals p2.ID
