6907cae236c99f19dc022d145fb326f0e6e9897a
Anton Bakhmet <abakhmet@gmail.com>
2011-03-31 13:19:16 +0300

    Style changes

--- UnitTests/Linq/IdlTest.cs
+++ UnitTests/Linq/IdlTest.cs
@@ -8,9 +8,9 @@ namespace Data.Linq
     [TestFixture]
     public class IdlTest : TestBase
     {
-        public struct StationObjectId
+        public struct ObjectId
         {
-            public StationObjectId(int value)
+            public ObjectId(int value)
             {
                 m_value = value;
             }
@@ -23,85 +23,88 @@ namespace Data.Linq
                 set { m_value = value; }
             }
 
-            public static implicit operator int(StationObjectId val)
+            public static implicit operator int(ObjectId val)
             {
                 return val.m_value;
             }
         }
 
-        private void TestJoin(Action<ITestDataContext> func)
+        [Test]
+        public void TestComplexExpression()
+        {
+            // failed with BLToolkit.Data.Linq.LinqException : 'new StationObjectId() {Value = ConvertNullable(child.ChildID)}' cannot be converted to SQL.
+            ForMySqlProvider(
+                db =>
+                {
+                    var dbm = db;
+
+                    var allEquities = from child in dbm.GrandChild
+                                      select
+                                          new
+                                          {
+                                              NullableId =
+                                                  child.ChildID == null
+                                                      ? (ObjectId?)null
+                                                      : new ObjectId { Value = child.ChildID.Value }
+                                          };
+
+                    var query = from e in allEquities where e.NullableId == 1 select e;
+
+                    var result = query.ToArray();
+                    Assert.That(result, Is.Not.Null);
+                });
+        }
+
+
+        [Test]
+        public void TestJoin()
+        {
+            // failed with System.ArgumentOutOfRangeException : Index was out of range. Must be non-negative and less than the size of the collection.
+            // Parameter name: index
+            ForMySqlProvider(
+                db =>
+                {
+                    var source = from p1 in db.Person
+                                 join p2 in db.Person on p1.ID equals p2.ID
+                                 select
+                                     new { ID1 = new ObjectId { Value = p1.ID }, FirstName2 = p2.FirstName, };
+
+                    var query = from p1 in source select p1.ID1.Value;
+
+                    var result = query.ToArray();
+                    Assert.That(result, Is.Not.Null);
+                });
+        }
+
+        [Test]
+        public void TestNullableExpression()
+        {
+            // failed with System.NullReferenceException : Object reference not set to an instance of an object.
+            ForMySqlProvider(
+                db =>
+                {
+                    var objects = from obj in db.Person select new { Id = obj.ID, };
+
+                    // fails for bool?, double?, int32?, int64?, string
+                    // works for byte?, int16?, DateTime? 
+                    double? @p1 = null;
+
+                    var r = from c in objects where @p1 != null select c;
+
+                    Assert.That(r.ToArray(), Is.Not.Null);
+                });
+        }
+
+        private void ForMySqlProvider(Action<ITestDataContext> func)
         {
             ForEachProvider(
                 new[]
                     {
                         "Sql2008", "Sql2005", ProviderName.SqlCe, ProviderName.DB2, ProviderName.Informix,
-                        ProviderName.Firebird, "Oracle", ProviderName.PostgreSQL, ProviderName.SQLite, ProviderName.Sybase
-                        , ProviderName.Access
-                    }, func);
-        }
-
-        // failed with BLToolkit.Data.Linq.LinqException : 'new StationObjectId() {Value = ConvertNullable(child.ChildID)}' cannot be converted to SQL.
-        [Test]
-        public void Join1()
-        {
-            TestJoin(
-                db =>
-                    {
-                        var dbm = db;
-
-                        var allEquities = from child in dbm.GrandChild
-                                          select
-                                              new
-                                              {
-                                                  NullableId =
-                                                      child.ChildID == null
-                                                          ? (StationObjectId?)null
-                                                          : new StationObjectId { Value = child.ChildID.Value }
-                                              };
-
-                        var query = from e in allEquities where e.NullableId == 1 select e;
-
-                        var result = query.ToArray();
-                        Assert.That(result, Is.Not.Null);
-                    });
-        }
-
-        // failed with System.NullReferenceException : Object reference not set to an instance of an object.
-        [Test]
-        public void Join2()
-        {
-            TestJoin(
-                db =>
-                    {
-                        var objects = from obj in db.Person select new { Id = obj.ID, };
-
-                        double? @p1 = null;
-
-                        var r = from c in objects where @p1 != null select c;
-
-                        Assert.That(r.ToArray(), Is.Not.Null);
-                    });
-        }
-
-        // failed with System.ArgumentOutOfRangeException : Index was out of range. Must be non-negative and less than the size of the collection.
-        // Parameter name: index
-        [Test]
-        public void Join3()
-        {
-            TestJoin(
-                db =>
-                    {
-                        var source = from p1 in db.Person
-                                     join p2 in db.Person on p1.ID equals p2.ID
-                                     select
-                                         new { ID1 = new StationObjectId { Value = p1.ID }, FirstName2 = p2.FirstName, };
-
-                        var query = from p1 in source
-                                    select p1.ID1.Value;
-
-                        var result = query.ToArray();
-                        Assert.That(result, Is.Not.Null);
-                    });
+                        ProviderName.Firebird, "Oracle", ProviderName.PostgreSQL, ProviderName.SQLite,
+                        ProviderName.Sybase, ProviderName.Access
+                    },
+                func);
         }
     }
 }
