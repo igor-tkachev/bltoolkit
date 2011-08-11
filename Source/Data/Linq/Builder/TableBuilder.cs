63ff58f1c7b5dc5bce25c37d84d88fa1f90c3afb
Igor Tkachev <igor.tkachev@gmail.com>
2011-08-11 16:26:52 -0400

    Fixed SL bug.

--- Source/Data/Linq/Builder/TableBuilder.cs
+++ Source/Data/Linq/Builder/TableBuilder.cs
@@ -866,7 +866,7 @@ namespace BLToolkit.Data.Linq.Builder
 
 					var predicate = Expression.Lambda<Func<T,bool>>(expr, param);
 
-					return Linq.Extensions.GetTable<T>(null).Where(predicate).Expression;
+					return association.Builder.DataContextInfo.DataContext.GetTable<T>().Where(predicate).Expression;
 				}
 			}
 
