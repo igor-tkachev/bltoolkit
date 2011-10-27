29d6816e1d5c8d00a34afcee20a775b94624e309
Igor Tkachev <igor.tkachev@gmail.com>
2011-10-27 02:12:06 -0400

    Working on DataService.

--- Source/ServiceModel/DataService.cs
+++ Source/ServiceModel/DataService.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Data.Services.Providers;
+using System.Linq;
 
 namespace BLToolkit.ServiceModel
 {
 	using Data.Linq;
+	using Data.Sql;
+	using Mapping;
 
 	public class DataService<T> : System.Data.Services.DataService<T>, IServiceProvider
 		where T : IDataContext
 	{
+		public DataService() : this(Map.DefaultSchema)
+		{
+		}
+
+		public DataService(MappingSchema mappingSchema)
+		{
+			_mappingSchema = mappingSchema;
+			_metadata = GetMetadataProvider(typeof(T));
+		}
+
+		readonly MappingSchema _mappingSchema;
+
 		public object GetService(Type serviceType)
 		{
 			if (serviceType == typeof(IDataServiceMetadataProvider))
@@ -20,15 +35,18 @@ namespace BLToolkit.ServiceModel
 			return null;
 		}
 
-		IDataServiceMetadataProvider _metadata;
-
-		public DataService()
-		{
-			_metadata = GetMetadataProvider(typeof(T)); 
-		}
+		readonly IDataServiceMetadataProvider _metadata;
 
 		public IDataServiceMetadataProvider GetMetadataProvider(Type dataSourceType)
 		{
+			var properties =
+				from p in dataSourceType.GetProperties()
+				let t = p.PropertyType
+				where t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Table<>)
+				select new SqlTable(_mappingSchema, t.GetGenericArguments()[0]);
+
+			var list = properties.ToList();
+
 			return null;
 		}
 	}
