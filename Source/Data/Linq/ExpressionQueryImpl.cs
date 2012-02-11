fedcfd83dc8c846cc42884a3c70b99197b52e267
Igor Tkachev <igor.tkachev@gmail.com>
2012-02-11 08:46:48 +0800

    Fixed mono isuue.

---
+++ Source/Data/Linq/ExpressionQueryImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace BLToolkit.Data.Linq
+{
+	class ExpressionQueryImpl<T> : ExpressionQuery<T>, IExpressionQuery
+	{
+		public ExpressionQueryImpl(IDataContextInfo dataContext, Expression expression)
+			: base(dataContext, expression)
+		{
+		}
+
+		//public new string SqlText
+		//{
+		//	get { return base.SqlText; }
+		//}
+
+#if OVERRIDETOSTRING
+
+		public override string ToString()
+		{
+			return base.SqlText;
+		}
+
+#endif
+	}
+}
