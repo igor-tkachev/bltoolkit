838f7f0e576d88c0da55a3008af7bd6eec646922
lvaleriu <lacatusu.valeriu@gmail.com>
2012-02-29 09:36:34 +0100

    Added abstract factory class

---
+++ Source/Data/DbConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using BLToolkit.Data.DataProvider;
+
+namespace BLToolkit.Data
+{
+    public abstract class DbConnectionFactory : IDbConnectionFactory
+    {
+        internal DataProviderBase Provider;
+        internal string ConnectionString { get; set; }
+
+        #region IDbConnectionFactory Members
+
+        public DbManager CreateDbManager()
+        {
+            return new DbManager(Provider, ConnectionString);
+        }
+
+        #endregion
+    }
+}
