00e74a673e6cca8e68d607927d20c586bc8ddfbd
Igor Tkachev <igor.tkachev@gmail.com>
2012-07-26 18:29:56 -0400

    Working on test source generator.

--- Source/Common/Configuration.cs
+++ Source/Common/Configuration.cs
@@ -78,9 +78,10 @@ namespace BLToolkit.Common
 
 		public static class Linq
 		{
-			public static bool PreloadGroups      { get; set; }
-			public static bool IgnoreEmptyUpdate  { get; set; }
-			public static bool AllowMultipleQuery { get; set; }
+			public static bool PreloadGroups                 { get; set; }
+			public static bool IgnoreEmptyUpdate             { get; set; }
+			public static bool AllowMultipleQuery            { get; set; }
+			public static bool GenerateTestSourceOnException { get; set; }
 		}
 
 		public static class NullableValues
