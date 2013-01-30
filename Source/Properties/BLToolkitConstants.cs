1139f1f201d72a0bf16965ae7eccc6fef2c9b40d
Igor Tkachev <igor.tkachev@gmail.com>
2013-01-30 16:38:08 -0500

    Updated version.

--- Source/Properties/BLToolkitConstants.cs
+++ Source/Properties/BLToolkitConstants.cs
@@ -20,7 +20,7 @@ namespace BLToolkit
 		/// <summary>
 		/// Build component of version.
 		/// </summary>
-		public const string Build = "16";
+		public const string Build = "17";
 
 		/// <summary>
 		/// Full version string.
