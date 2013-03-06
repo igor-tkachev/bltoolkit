9b8222bed71267f04d4b4ef595cbf00357c51802
Igor Tkachev <igor.tkachev@gmail.com>
2013-03-05 20:36:34 -0500

    Fixed SelectMany/Update issue.

--- UnitTests/Linq/UserTests/SelectManyDeleteTest.cs
+++ UnitTests/Linq/UserTests/SelectManyDeleteTest.cs
@@ -11,35 +11,35 @@ using NUnit.Framework;
 
 namespace Data.Linq.UserTests
 {
-	[TableName(Name = "GrandChild")]
-	public class GrandChild
-	{
-		public int ChildID { get; set; }
-	}
-
-	[TableName(Name = "Child")]
-	public class Child
-	{
-		public int ParentID { get; set; }
-		public int ChildID  { get; set; }
-
-		[Association(ThisKey = "ChildID", OtherKey = "ChildID", CanBeNull = false)]
-		public List<GrandChild> GrandChildren { get; set; }
-	}
-
-	[TableName(Name = "Parent")]
-	public class Parent
-	{
-		[Identity, PrimaryKey(1)]
-		public int ParentID { get; set; }
-
-		[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true)]
-		public List<Child> Children { get; set; }
-	}
-
 	[TestFixture]
 	public class SelectManyDeleteTest : TestBase
 	{
+		[TableName(Name = "GrandChild")]
+		public new class GrandChild
+		{
+			public int ChildID { get; set; }
+		}
+
+		[TableName(Name = "Child")]
+		public new class Child
+		{
+			public int ParentID { get; set; }
+			public int ChildID  { get; set; }
+
+			[Association(ThisKey = "ChildID", OtherKey = "ChildID", CanBeNull = false)]
+			public List<GrandChild> GrandChildren { get; set; }
+		}
+
+		[TableName(Name = "Parent")]
+		public new class Parent
+		{
+			[Identity, PrimaryKey(1)]
+			public int ParentID { get; set; }
+
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true)]
+			public List<Child> Children { get; set; }
+		}
+
 		[Test]
 		public void Test([DataContexts(
 			ProviderName.Access, ProviderName.DB2, ProviderName.Informix, "Oracle",
