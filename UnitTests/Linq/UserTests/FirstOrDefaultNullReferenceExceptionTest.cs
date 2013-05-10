8e7d7782f441c686386f2f03c6b6906bf68f50aa
Igor Tkachev <igor.tkachev@gmail.com>
2013-05-09 22:49:47 -0400

    Fixed test.

--- UnitTests/Linq/UserTests/FirstOrDefaultNullReferenceExceptionTest.cs
+++ UnitTests/Linq/UserTests/FirstOrDefaultNullReferenceExceptionTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using BLToolkit.DataAccess;
 using BLToolkit.Mapping;
 
 using NUnit.Framework;
@@ -11,26 +11,29 @@ namespace Data.Linq.UserTests
 	[TestFixture]
 	public class FirstOrDefaultNullReferenceExceptionTest : TestBase
 	{
+		[TableName("GrandChild")]
 		class Table1
 		{
-			public int Field1 { get; set; }
+			public int ChildID;
 		}
 
+		[TableName("Child")]
 		class Table2
 		{
-			public int Field1 { get; set; }
-			public int Field2 { get; set; }
+			public int ChildID;
+			public int ParentID;
 
-			[Association(ThisKey = "Field1", OtherKey = "Field1", CanBeNull = true)]
-			public List<Table1> Table1s { get; set; }
+			[Association(ThisKey = "ChildID", OtherKey = "ChildID", CanBeNull = true)]
+			public List<Table1> GrandChildren { get; set; }
 		}
 
+		[TableName("Parent")]
 		class Table3
 		{
-			public int Field2 { get; set; }
+			public int ParentID { get; set; }
 
-			[Association(ThisKey = "Field2", OtherKey = "Field2", CanBeNull = true)]
-			public List<Table2> Table2s { get; set; }
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true)]
+			public List<Table2> Children { get; set; }
 		}
 
 		[Test]
@@ -52,15 +55,15 @@ namespace Data.Linq.UserTests
 
 				var query =
 					from t3 in db.GetTable<Table3>()
-					let t1 = t3.Table2s.SelectMany(x => x.Table1s)
+					let t1 = t3.Children.SelectMany(x => x.GrandChildren)
 					//let t2 = t3.Table2s.SelectMany(x => x.Table1s)
 					select new
 					{
 						c2 = t1.Count(),
-						c1 = t3.Table2s.SelectMany(x => x.Table1s).Count(),
+						c1 = t3.Children.SelectMany(x => x.GrandChildren).Count(),
 					};
 
-				query.FirstOrDefault();
+				query.FirstOrDefault(p => p.c1 > 10);
 			}
 		}
 	}
