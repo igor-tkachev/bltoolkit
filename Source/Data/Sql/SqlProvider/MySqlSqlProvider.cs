ab8b3480c5a0aa9bd25a393a4773507a31398446
Igor Tkachev <igor.tkachev@gmail.com>
2013-06-12 19:55:04 -0400

    Working on #236.

--- Source/Data/Sql/SqlProvider/MySqlSqlProvider.cs
+++ Source/Data/Sql/SqlProvider/MySqlSqlProvider.cs
@@ -301,5 +301,35 @@ namespace BLToolkit.Data.Sql.SqlProvider
 		{
 			sb.AppendLine("() VALUES ()");
 		}
+
+		public static bool GenerateOldGuid = false;
+
+		public override void BuildValue(StringBuilder sb, object value)
+		{
+			if (GenerateOldGuid && value is Guid)
+			{
+				var bytes = ((Guid)value).ToByteArray();
+
+				sb.Append("X'").Append(ByteArrayToHex(bytes)).Append('\'');
+			}
+			else
+				base.BuildValue(sb, value);
+		}
+
+		static string ByteArrayToHex(byte[] barray)
+		{
+			var c = new char[barray.Length * 2];
+
+			for (var i = 0; i < barray.Length; ++i)
+			{
+				var b = ((byte)(barray[i] >> 4));
+
+				c[i * 2] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+				b = ((byte)(barray[i] & 0xF));
+				c[i * 2 + 1] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+			}
+
+			return new string(c);
+		}
 	}
 }
