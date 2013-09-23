28f1e83ab39032cff0fae78bf647a97204c53dbf
lvaleriu <lacatusu.valeriu@gmail.com>
2013-09-23 18:28:13 +0200

    Version -> 4.1.21.11

--- Source/Properties/BLToolkitConstants.cs
+++ Source/Properties/BLToolkitConstants.cs
@@ -42,6 +42,6 @@ namespace BLToolkit
         // <summary>
         // Revision component of version.
         // <summary>
-        public const string AutoRevision = "10";
+        public const string AutoRevision = "11";
     }
 }
