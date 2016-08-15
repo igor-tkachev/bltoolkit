affa12b7caff672a54c4d6a0cfac8dff3eaef52b
ili <chudinia@gmail.com>
2016-08-15 08:31:31 +0500

    working on #381
    * MySql

--- Data/Create Scripts/MySql.sql
+++ Data/Create Scripts/MySql.sql
@@ -464,7 +464,10 @@ CREATE TABLE LinqDataTypes
 	BinaryValue    varbinary(5000) NULL,
 	SmallIntValue  smallint,
 	IntValue       int NULL,
-	BigIntValue    bigint NULL
+	BigIntValue    bigint NULL,
+	UInt16         decimal(5,  0)  NULL,
+	UInt32         decimal(10, 0)  NULL,
+	UInt64         decimal(20, 0)  NULL
 )
 GO
 
